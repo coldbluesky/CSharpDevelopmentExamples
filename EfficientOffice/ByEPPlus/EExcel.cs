f17985d7ad2010db8633b7b3e0442f11cf5944f0
ldy <870588618@qq.com>
2023-08-31 15:15:26 +0800

    添加创建空excel，设置excel密码，清除excel密码

--- EfficientOffice/ByEPPlus/EExcel.cs
+++ EfficientOffice/ByEPPlus/EExcel.cs
@@ -1,9 +1,71 @@
-﻿using OfficeOpenXml;
+﻿using Microsoft.Office.Interop.Excel;
+using OfficeOpenXml;
 
 namespace EfficientOffice.ByEPPlus
 {
+    /// <summary>
+    /// 用EPPlus实现的excel操作,不支持xls格式
+    /// 6.2官方文档地址:https://epplussoftware.com/docs/6.2/api/index.html
+    /// </summary>
     public static class EExcel
     {
-        
+        public static ExcelPackage EInstance()
+        {
+            SetLicenseContext();
+            return new ExcelPackage();
+        }
+        public static ExcelPackage EInstance(string path)
+        {
+            SetLicenseContext();
+            return new ExcelPackage(path);
+        }
+        public static ExcelPackage EInstance(string path, string pwd)
+        {
+            SetLicenseContext();
+            return new ExcelPackage(path, pwd);
+        }
+        public static void SetLicenseContext()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+        }
+
+        public static void CreateEmpty(string path)
+        {
+            using (var package = EInstance())
+            {
+                var sheet = package.Workbook.Worksheets.Add("sheet1");
+                package.SaveAs(Path.Combine(path, "empty.xlsx"));
+            }
+        }
+        /// <summary>
+        /// 第一次设置密码
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="Pwd"></param>
+        public static void SetPassword(string path, string pwd)
+        {
+            using (var package = EInstance(path))
+            {
+                package.Save(pwd);
+            }
+        }
+        public static void SetPassword(string path, string newPwd, string oldPwd)
+        {
+            using (var package = EInstance(path,oldPwd))
+            {
+                package.Save(newPwd);
+            }
+        }
+
+        public static void ClearPassword(string path, string pwd)
+        {
+            using (var package = EInstance(path, pwd))
+            {
+                package.Encryption.Password = null;
+                package.Save();
+            }
+        }
     }
+
 }
--- Main/Program.cs
+++ Main/Program.cs
@@ -1,18 +1,16 @@
 ﻿
 using FileOperations;
 using FileOperations.Enum;
-
+using EfficientOffice.ByEPPlus;
 namespace CSharpDevelopmentExamples
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<string> list = new List<string>();
             string path = @"C:\Users\Administrator\Desktop\book";
-            Console.WriteLine(BasicFileOperations.INIRead("server", "RemotelP", path));
-            //BasicFileOperations.CreateXml(@"C:\Users\Administrator\Desktop");
-            Console.WriteLine(BasicFileOperations.FileCommuniteUnite(@"C:\Users\Administrator\Desktop\book\新建文件夹", @"C:\Users\Administrator\Desktop\book\新建文件夹\new.pdf"));
+            //EExcel.CreateEmpty(path);
+            EExcel.ClearPassword(Path.Combine(path,"empty.xlsx"),"234");
 
         }
     }
